5cd7bf4b679065c8ad25d936baface6428c7f545
Jacopo Credi <jacopo.credi@gmail.com>
2015-07-16 18:01:14 +0200

    ciao micia

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -26,9 +26,10 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
+m = size(X,1);
+mu = mean(X);
+sigma = std(X);
+X_norm = (X-repmat(mu,[m 1]))./repmat(sigma,[m 1]);
 
 
 
